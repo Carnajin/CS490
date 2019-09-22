25523d76a0477c2dbf27756b809b0fbc0d8668a9
Carnajin <55556867+Carnajin@users.noreply.github.com>
2019-09-22 12:32:32 -0400

    Update login.php

--- back/login.php
+++ back/login.php
@@ -1,12 +1,10 @@
 <?php
 $json_str = file_get_contents('php://input');
 $output = json_decode($json_str, true);
-$username="none";
-$password="none";
-
-if(isset($output['username'])) $username = $output['username'];
-if(isset($output['password'])) $username = $output['password'];
-
+$usId="none";
+$pass="none";
+if(isset($output['username'])) $usId = $output['username'];
+if(isset($output['password'])) $pass = $output['password'];
 include("account.php");
 $db = mysqli_connect($host,$username,$password,$project) OR die(mysqli_connect_error());
 mysqli_set_charset($db,'utf8');
@@ -16,12 +14,32 @@ if(mysqli_connect_error()){
 }
 print "Success";
 mysqli_select_db($db,$project);
-if($_POST['user'] $$ $_POST['password']){
-    $pass = $_POST['password'];
-    $uId = $_POST['user'];
-}
+
 $hash = password_hash($pass,PASSWORD_DEFAULT);
-$p = "update users set pwdUsers = '$hash' where uidUsers = '$uId'";
+$p = "update users set pwdUsers = '$hash' where uidUsers = '$usId'";
 ($t = mysqli_query($db, $p)) or die (mysqli_error($db));
 mysqli_close($db);
+/*verify password
+function authenticate ($usId,$pass,$db) {
+        $s = "select * from users where uidUsers='$usId'";
+       
+        ($t = mysqli_query($db, $s)) or die (mysqli_error($db));
+        $num = mysqli_num_rows($t);
+        if($num == 0) {
+            return false;
+        }
+        else {
+            $r = mysqli_fetch_array($t,MYSQLI_ASSOC);
+            $h = $r['pwdUsers'];
+            if(password_verify($pass,$h)){
+                echo "Password is valid<br>";
+                return true;
+            }else{
+                echo "Invalid password<br>";
+                return false;
+            }
+        };
+    }
+send curl back
+*/
 ?>
