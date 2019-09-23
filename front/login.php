33576374cbd34db10b4d7764ef526dcc8142807d
Carnajin <55556867+Carnajin@users.noreply.github.com>
2019-09-23 18:07:54 -0400

    Update login.php

--- front/login.php
+++ front/login.php
@@ -13,7 +13,7 @@ print "<h2>".$credentials."</h2>"; // a confirm statement of what was passed
 
 function pass_credentials($username,$password){
     $data = array("username" => $username, "password" =>$password); // var 1
-    $url = "test/index.html"; // var 2
+    $url = "https://web.njit.edu/~vnp27/cs490/mid/login.php"; // var 2
     $data_string = http_build_query($data);
     $ch = curl_init($url); // init the curl function 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
