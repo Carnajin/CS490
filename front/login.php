5fa9ee6e9ac1c415a75507daf847e3e10e515107
Carnajin <55556867+Carnajin@users.noreply.github.com>
2019-09-25 17:41:57 -0400

    Updated

--- front/login.php
+++ front/login.php
@@ -1,14 +1,19 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $username = $_POST["ucid"];
 $password = $_POST["pass"];
 
-$info = ["ucid" => $username, "pass" => $pwd];
+$info = ["ucid" => $username, "pass" => $password];
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_POSTFIELDS, $info);
 curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~vnp27/cs490/mid/login.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $response = curl_exec($ch);
-
+curl_close($ch);
 echo $response;
+
 ?>
