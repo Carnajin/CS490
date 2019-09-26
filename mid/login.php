9e7ac1d5d8ab951f088a9067654e28b2a34476a9
Pranay-Mistry <49734396+Pranay-Mistry@users.noreply.github.com>
2019-09-25 23:11:39 -0400

    Add files via upload

--- mid/login.php
+++ mid/login.php
@@ -6,50 +6,47 @@ error_reporting(E_ALL);
 
 $username = $_POST["ucid"];
 $password = $_POST["pass"];
-
 $res_project=login_project($username,$password);
-echo $res_project;
 function login_project($username,$password)
 {
-	$data = array('ucid' => $username,'pass' =>$password);
-	$url = "https://web.njit.edu/~pm458/cs490/back/login.php";
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	$output = curl_exec($ch);
-	curl_close ($ch);
+ 	$data = array('ucid' => $username,'pass' =>$password);
+ 	$url = "https://web.njit.edu/~pm458/cs490/back/login.php";
+ 	$ch = curl_init();
+ 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
+ 	curl_setopt($ch, CURLOPT_URL, $url);
+ 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ 	$output = curl_exec($ch);
+ 	curl_close ($ch);
 	return $output;
 
-    //vnp27
+
 }
 
  //curl njit
-
 $njitlogin = login_njit($username, $password);
 
 function login_njit($username,$password)
-{
-  
-	$url = "https://aevitepr2.njit.edu/MyHousing/login.cfm";
-	$data = array('ucid' => $username,'pass' =>$password);
-	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data); 
-	$output = curl_exec($ch);
-  if(strpos($output,"welcome") == true){
-    $out = array("njit"=>1);
-  }
-  else{
-    $out = array("njit"=>0);
-  }
-	curl_close ($ch);
+{  
+ 	$url = "https://aevitepr2.njit.edu/MyHousing/login.cfm";
+ 	$data = array('ucid' => $username,'pass' =>$password);
+ 	$ch = curl_init();
+ 	curl_setopt($ch, CURLOPT_URL, $url);
+ 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ 	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+   curl_setopt($ch, CURLOPT_FOLLOWLOCATION,1);
+ 	$output = curl_exec($ch);
+   if(strpos($output,"Please select a MyHousing") == true){
+     $out = 1;
+   }
+   else{
+     $out = 0;
+}
+curl_close ($ch);
  
 	return $out;
 }
-
-
-echo json_encode($njitlogin);
+$res = array("njit" => $njitlogin, "back" => $res_project);
+$respond = json_encode($res);
+ echo $respond;
 ?>
 
