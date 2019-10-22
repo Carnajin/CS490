27d8d195b8cb68ad2d3ac6776ab9e4a364180a6c
Carnajin <55556867+Carnajin@users.noreply.github.com>
2019-10-22 10:46:51 -0400

    Updated

--- Beta/front/curlhandler.php
+++ Beta/front/curlhandler.php
@@ -1,35 +1,15 @@
 <?php
-//*****receive post data from user based on message post field*****
-
+//---------Message for cases-------------------
 $message = $_POST["message"];
-
+//---------Cases for sending POST data-----------------------
 switch($message) {
   
-  case "CreateQuestion":
-    $testCase1 = $_POST["tcs1"];
-    $testCase2 = $_POST["tcs2"];
-    $funcName = $_POST["question"];
-    $description = $_POST["description"];
-    $difficulty = $_POST["difficulty"];
-    $topic = $_POST["topic"];
-    
-    $comp = array(
-      "message" => $message,
-      "tcs1" => $testCase1,
-      "tcs2" => $testCase2,
-      "question" => $funcName,
-      "description" => $description,
-      "difficulty" => $difficulty,
-      "topic" => $topic
-    );
-    break;
-
   case "CreateExam":    
     $examName = $_POST["examName"];
     $questionID = $_POST["questionID"];
     $points = $_POST["points"];
     
-    $comp = array(
+    $data = array(
       "message" => $message,
       "examName" => $examName,
       "questionID" => $questionID,
@@ -37,38 +17,57 @@ switch($message) {
     );
     break;
 
+  case "CreateQuestion":
+    $testCase1 = $_POST["tcs1"];
+    $testCase2 = $_POST["tcs2"];
+    $question = $_POST["question"];
+    $description = $_POST["description"];
+    $difficulty = $_POST["difficulty"];
+    $topic = $_POST["topic"];
+    
+    $data = array(
+      "message" => $message,
+      "tcs1" => $testCase1,
+      "tcs2" => $testCase2,
+      "question" => $question,
+      "description" => $description,
+      "difficulty" => $difficulty,
+      "topic" => $topic
+    );
+    break;
+
   case "ReleaseExam":
-    $examId = $_POST["examId"];
+    $examID = $_POST["examId"];
     $questionID = $_POST["questionID"];
     $comments = $_POST["comments"];
     $grade = $_POST["grade"];
     $loop = $_POST["loop"];
     $return = $_POST["return"];
-    $funcName = $_POST["funcName"];
+    $question = $_POST["funcName"];
     $extra = $_POST["extra"];
    
-    $comp = array(
+    $data = array(
       "message" => $message,
-      "examId" => $examId,
+      "examId" => $examID,
       "questionID" => $questionID,
       "comments" => $comments,
       "grade" => $grade,
       "loop" => $loop,
       "return" => $return,
-      "funcName" => $funcName,
+      "funcName" => $question,
       "extra" => $extra
     );
     break;
 
   case "TakeExam":
-    $examId = $_POST["examId"];
+    $examID = $_POST["examId"];
     $questionID = $_POST["questionID"];
     $code = $_POST["code"];
     $points = $_POST["points"];
     
-    $comp = array(
+    $data = array(
       "message" => $message,
-      "examId" => $examId,
+      "examId" => $examID,
       "questionID" => $questionID,
       "code" => $code,
       "points" => $points
@@ -76,43 +75,43 @@ switch($message) {
     break;
     
   case "StudentViewExam":
-    $examId = $_POST["examId"];
+    $examID = $_POST["examId"];
   
-    $comp = array(
+    $data = array(
       "message" => $message,
-      "examId" => $examId
+      "examId" => $examID
+    );
+    break;
+
+  case "InstructorViewExam":
+    $examID = $_POST["examId"];
+    
+    $data = array(
+      "message" => $message,
+      "examId" => $examID 
     );
     break;
 
   case "GetExam":
     $examId = $_POST["examId"];
   
-    $comp = array(
+    $data = array(
       "message" => $message,
-      "examId" => $examId
+      "examId" => $examID
     );
     break;
 
   case "GetQuestions":
     $id = $_POST[$id];
   
-    $comp = array(
+    $data = array(
       "message" => $message,
       "id" => $id
     );
     break;
-    
-  case "InstructorViewExam":
-    $examId = $_POST["examId"];
-    
-    $comp = array(
-      "message" => $message,
-      "examId" => $examId 
-    );
-    break;
   
   default:
-    echo "defaulted case";
+    echo "Error: Check Cases!";
 }
 
 //------------CURL Request to Mid--------------------------------------------
@@ -120,7 +119,7 @@ switch($message) {
 $ch = curl_init("https://web.njit.edu/~pm458/cs490/back/logic.php");
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $comp);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
