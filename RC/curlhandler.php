cbb65caa0bee1493c537e5b85c2f7e39600bca1c
Carnajin <55556867+Carnajin@users.noreply.github.com>
2019-11-03 11:30:41 -0500

    RC Folder Created

---
+++ RC/curlhandler.php
@@ -0,0 +1,147 @@
+<?php
+//---------Message for cases-------------------
+$operation = $_POST["message"];
+
+//---------Cases for sending POST data-----------------------
+switch($operation) {
+  
+  case "CreateExam":    
+    $examName = $_POST["examName"];
+    $questionID = $_POST["questionID"];
+    $points = $_POST["points"];
+    
+    $data = array(
+      "message" => $operation,
+      "examName" => $examName,
+      "questionID" => $questionID,
+      "points" => $points,
+    );
+    break;
+
+  case "CreateQuestion":
+    $testCase1 = $_POST["tcs1"];
+    $testCase2 = $_POST["tcs2"];
+    $testCase2 = $_POST["tcs3"];
+    $testCase2 = $_POST["tcs4"];
+    $testCase2 = $_POST["tcs5"];
+    $testCase2 = $_POST["tcs6"];
+    $testCaseResult1 = $_POST["tcsr1"];
+    $testCaseResult2 = $_POST["tcsr2"];
+    $testCaseResult2 = $_POST["tcsr3"];
+    $testCaseResult2 = $_POST["tcsr4"];
+    $testCaseResult2 = $_POST["tcsr5"];
+    $testCaseResult2 = $_POST["tcsr6"];
+    $question = $_POST["question"];
+    $description = $_POST["description"];
+    $difficulty = $_POST["difficulty"];
+    $topic = $_POST["topic"];
+    
+    $data = array(
+      "message" => $operation,
+      "tcs1" => $testCase1,
+      "tcs2" => $testCase2,
+      "tcs3" => $testCase3,
+      "tcs4" => $testCase4,
+      "tcs5" => $testCase5,
+      "tcs6" => $testCase6,
+      "tcsr1" => $testCaseResult1,
+      "tcsr2" => $testCaseResult2,
+      "tcsr3" => $testCaseResult3,
+      "tcsr4" => $testCaseResult4,
+      "tcsr5" => $testCaseResult5,
+      "tcsr6" => $testCaseResult6,
+      "question" => $question,
+      "description" => $description,
+      "difficulty" => $difficulty,
+      "topic" => $topic
+    );
+    break;
+
+  case "ReleaseExam":
+    $examID = $_POST["examId"];
+    $questionID = $_POST["questionID"];
+    $comments = $_POST["comments"];
+    $grade = $_POST["grade"];
+    $fname = $_POST["fName"];
+    $return = $_POST["return"];
+   
+    $data = array(
+      "message" => $operation,
+      "examId" => $examID,
+      "questionID" => $questionID,
+      "comments" => $comments,
+      "grade" => $grade,
+      "fName" => $fname,
+      "return" => $return
+    );
+    break;
+
+  case "TakeExam":
+    $examName = $_POST["examName"];
+    $examID = $_POST["examId"];
+    $questionA = $_POST["questionA"];
+    $code = $_POST["code"];
+    $points = $_POST["points"];
+    
+    $data = array(
+      "message" => $operation,
+      "examName" => $examName,
+      "examId" => $examID,
+      "questionA" => $questionA,
+      "code" => $code,
+      "points" => $points
+    );
+    break;
+    
+  case "StudentViewExam":
+    $examID = $_POST["examId"];
+  
+    $data = array(
+      "message" => $operation,
+      "examId" => $examID
+    );
+    break;
+
+  case "InstructorViewExam":
+    $examID = $_POST["examId"];
+    
+    $data = array(
+      "message" => $operation,
+      "examId" => $examID 
+    );
+    break;
+
+  case "GetExam":
+    $examId = $_POST["examId"];
+  
+    $data = array(
+      "message" => $operation,
+      "examId" => $examID
+    );
+    break;
+
+  case "GetQuestions":
+    $id = $_POST[$id];
+  
+    $data = array(
+      "message" => $operation,
+      "id" => $id
+    );
+    break;
+  
+  default:
+    echo "Error: Check Cases!";
+}
+
+//------------CURL Request to Mid--------------------------------------------
+
+$ch = curl_init("https://web.njit.edu/~vnp27/cs490/mid/curlhandler.php");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+echo $response;
+?>
